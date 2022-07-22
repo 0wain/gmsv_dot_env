f6a65b540991cbfe8526c91f114685113aa545e7
Owain <owain.jones74@gmail.com>
2022-07-22 23:07:30 +0100

    fix weird spacing

--- src/lib.rs
+++ src/lib.rs
@@ -14,25 +14,25 @@ unsafe extern "C-unwind" fn read_env(lua: gmod::lua::State) -> i32 {
 
     if env_vars.contains_key(&lookup_var) {
         let desired_var = &env_vars[&lookup_var];
-		lua.push_string(desired_var);
+        lua.push_string(desired_var);
     } else {
         lua.push_nil();
     }
 
-	return 1;
+    return 1;
 }
 
 #[gmod13_open]
 pub unsafe extern "C-unwind" fn gmod13_open(lua: gmod::lua::State) -> i32 {
-	lua.push_function(read_env);
-	lua.set_global(lua_string!("env"));
+    lua.push_function(read_env);
+    lua.set_global(lua_string!("env"));
 
-	lua.pop();
+    lua.pop();
 
-	return 0;
+    return 0;
 }
 
 #[gmod13_close]
 fn gmod13_close(lua: gmod::lua::State) -> i32 {
-	return 0;
+    return 0;
 }
